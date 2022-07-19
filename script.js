a33b99876cc28471fbe0364a5a68f026c21d925b
Sailikhith1 <109460203+Sailikhith1@users.noreply.github.com>
2022-07-19 14:44:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ function checkImageWithNyckel(image)
     formdata.append('file', image);
 
     $.ajax({
-        url:'https://www.nyckel.com/v1/functions/pxvrvn451z7vtd44/invoke',
+        url:'https://www.nyckel.com/v1/functions/mmms93kdf5oy0kpp/invoke',
         type: 'post',
         data: formdata,
         contentType: false,
